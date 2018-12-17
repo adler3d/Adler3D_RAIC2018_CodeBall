003dbea82aab5ee780e2bf2591279154e708a835
adler3d <adler3d@gmail.com>
2018-12-18 02:25:07 +0300

    Update LR.node.js

--- LR/LR.node.js
+++ LR/LR.node.js
@@ -1,5 +1,6 @@
-var spawn=(app,args)=>require('child_process').spawn(app,args,{stdio:'inherit'});
-spawn("./codeball2018.exe","--p1 tcp-31001 --p2 tcp-31002 --team-size 2 --duration 20000 --nitro false".split(" "));
+var spawn=require('child_process').spawn;
+var params={stdio:'inherit'};
+spawn("./codeball2018.exe","--p1 tcp-31001 --p2 tcp-31002 --team-size 2 --duration 20000 --nitro false".split(" "),params);
 setTimeout(()=>{
   spawn("./app.exe","127.0.0.1 31001 9000".split(" "));
   spawn("./app.exe","127.0.0.1 31002 9000".split(" "));
